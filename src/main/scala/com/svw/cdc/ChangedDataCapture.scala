cd167de831e78be9e688ac79f3d9f45b4eb4d9e7
xinplv <xiaopeng1220>
2020-06-04 18:32:29 +0800

    调整代码结构-> V1.0

---
+++ src/main/scala/com/svw/cdc/ChangedDataCapture.scala
@@ -0,0 +1,97 @@
+package com.svw.cdc
+
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kudu.spark.kudu.KuduContext
+import org.apache.spark.SparkConf
+import org.apache.spark.sql.{AnalysisException, SparkSession}
+import org.apache.spark.streaming.kafka010.KafkaUtils
+//import org.apache.spark.streaming.kafka.KafkaUtils
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+/**
+ * root
+ * |-- payload: struct (nullable = true)
+ * |    |-- after: struct (nullable = true)
+ * |    |    |-- email: string (nullable = true)
+ * |    |    |-- first_name: string (nullable = true)
+ * |    |    |-- id: long (nullable = true)
+ * |    |    |-- last_name: string (nullable = true)
+ * |    |-- before: struct (nullable = true)
+ * |    |    |-- email: string (nullable = true)
+ * |    |    |-- first_name: string (nullable = true)
+ * |    |    |-- id: long (nullable = true)
+ * |    |    |-- last_name: string (nullable = true)
+ * |    |-- op: string (nullable = true)
+ * |    |-- source: struct (nullable = true)
+ * |    |    |-- db: string (nullable = true)
+ * |    |    |-- file: string (nullable = true)
+ * |    |    |-- gtid: string (nullable = true)
+ * |    |    |-- name: string (nullable = true)
+ * |    |    |-- pos: long (nullable = true)
+ * |    |    |-- row: long (nullable = true)
+ * |    |    |-- server_id: long (nullable = true)
+ * |    |    |-- snapshot: string (nullable = true)
+ * |    |    |-- table: string (nullable = true)
+ * |    |    |-- thread: long (nullable = true)
+ * |    |    |-- ts_sec: long (nullable = true)
+ * |    |-- ts_ms: long (nullable = true)
+ * |    |-- name: string (nullable = true)
+ * |    |-- optional: boolean (nullable = true)
+ * |    |-- type: string (nullable = true)
+ * |    |-- version: long (nullable = true)
+ */
+object ChangedDataCapture {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().appName("cdc-test").config(new SparkConf()).getOrCreate()
+    val ssc = new StreamingContext(spark.sparkContext, Seconds(5))
+    val kafkaParams = Map[String, Object]("bootstrap.servers" -> "10.160.240.244:9092"
+      , "auto.offset.reset" -> "earliest"
+      , "key.deserializer" -> classOf[StringDeserializer]
+      , "value.deserializer" -> classOf[StringDeserializer]
+      , "group.id" -> "test-consumer-group"
+    )
+
+    val topic = Array("cdc.cdc.tb_cdc")
+    val kudu_tb_name = "impala::tmp.tb_cdc"
+    val kuduMaster = "svlhdp004.csvw.com:7051,svlhdp005.csvw.com:7051,svlhdp006.csvw.com:7051"
+
+    val dStream = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topic, kafkaParams))
+
+    val kuduContext = new KuduContext(kuduMaster, spark.sparkContext)
+
+    if (!kuduContext.tableExists("impala::tmp.tb_cdc")) {
+      println("The Kudu Table :{impala::tmp.tb_cdc} does not exist.")
+    }
+    // 处理流 Kafka
+    dStream.map(record => record.value()).foreachRDD(rdd => {
+      if (!rdd.isEmpty()) {
+        val spark = SparkSession.builder().config(rdd.sparkContext.getConf).getOrCreate()
+        import spark.implicits._
+        // 解析json消息并转化为DataFrame
+        val cdcDF = spark.read.json(spark.createDataset(rdd))
+        try {
+          // 抽取msg里的"op"，用于判断该msg是哪种操作
+          val operation = cdcDF.select("payload.op").first().getString(0)
+          println("-----------------" + operation + "------------------------------")
+          if (operation == "u" || operation == "c") {
+            // 抽取msg里的相关业务字段并转化为DataFrame
+            val afterDF = cdcDF.select("payload.after.id", "payload.after.firstname", "payload.after.lastname", "payload.after.email")
+              .toDF("id", "firstname", "lastname", "email")
+            println("-----------------" + afterDF + "------------------------------")
+            // 更新数据到Kudu相应的表
+            kuduContext.upsertRows(afterDF, kudu_tb_name)
+          }
+        } catch {
+          case exception: AnalysisException =>
+            println("oooops,crashed")
+        }
+      }
+    })
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
--- src/main/scala/com/svw/cdc/Test.scala
+++ src/main/scala/com/svw/cdc/Test.scala
@@ -4,52 +4,64 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kudu.spark.kudu.KuduContext
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.{StringType, StructField, StructType}
-import org.apache.spark.streaming.kafka010.KafkaUtils
-//import org.apache.spark.streaming.kafka.KafkaUtils
 import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+import org.apache.spark.streaming.kafka010.KafkaUtils
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-import scala.util.parsing.json.JSON
-
+/**
+ * root
+ * |-- payload: struct (nullable = true)
+ * |    |-- after: struct (nullable = true)
+ * |    |    |-- email: string (nullable = true)
+ * |    |    |-- first_name: string (nullable = true)
+ * |    |    |-- id: long (nullable = true)
+ * |    |    |-- last_name: string (nullable = true)
+ * |    |-- before: struct (nullable = true)
+ * |    |    |-- email: string (nullable = true)
+ * |    |    |-- first_name: string (nullable = true)
+ * |    |    |-- id: long (nullable = true)
+ * |    |    |-- last_name: string (nullable = true)
+ * |    |-- op: string (nullable = true)
+ * |    |-- source: struct (nullable = true)
+ * |    |    |-- db: string (nullable = true)
+ * |    |    |-- file: string (nullable = true)
+ * |    |    |-- gtid: string (nullable = true)
+ * |    |    |-- name: string (nullable = true)
+ * |    |    |-- pos: long (nullable = true)
+ * |    |    |-- row: long (nullable = true)
+ * |    |    |-- server_id: long (nullable = true)
+ * |    |    |-- snapshot: string (nullable = true)
+ * |    |    |-- table: string (nullable = true)
+ * |    |    |-- thread: long (nullable = true)
+ * |    |    |-- ts_sec: long (nullable = true)
+ * |    |-- ts_ms: long (nullable = true)
+ * |    |-- name: string (nullable = true)
+ * |    |-- optional: boolean (nullable = true)
+ * |    |-- type: string (nullable = true)
+ * |    |-- version: long (nullable = true)
+ */
 object Test {
-
-  /*val cdcSchema = StructType(
-    StructField("id", StringType, nullable = false) ::
-      StructField("first_name", StringType, nullable = true) ::
-      StructField("last_name", StringType, nullable = true) ::
-      StructField("email", StringType, nullable = true) :: Nil
-  )
-
-  case class Cdc(
-                  id: String,
-                  first_name: String,
-                  last_name: String,
-                  email: String
-                )*/
 
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession.builder().appName("cdc-test").config(new SparkConf()).getOrCreate()
-//    val ssc = new StreamingContext(spark.sparkContext, Seconds(5))
-    val kafkaParams = Map[String, Object]("bootstrap.servers" -> "172.20.98.138:9091"
+    val ssc = new StreamingContext(spark.sparkContext, Seconds(5))
+    val kafkaParams = Map[String, Object]("bootstrap.servers" -> "10.160.240.244:9092"
       , "auto.offset.reset" -> "latest"
       , "key.deserializer" -> classOf[StringDeserializer]
       , "value.deserializer" -> classOf[StringDeserializer]
-      , "group.id" -> ""
-      , "auto.offset.reset" -> "earliest"
+      , "group.id" -> "test-consumer-group"
     )
 
-    val topic = Array("test")
+    val topic = Array("cdc.cdc.tb_cdc")
     val kuduMaster = "svlhdp004.csvw.com:7051,svlhdp005.csvw.com:7051,svlhdp006.csvw.com:7051"
     println("print---------------------------------start conn stream")
-   /* val dStream = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topic, kafkaParams))
+    val dStream = KafkaUtils.createDirectStream[String, String](ssc, PreferConsistent, Subscribe[String, String](topic, kafkaParams))
     println("print---------------------------------connected")
     println("print---------------------------------stream")
     println(dStream)
-    println(dStream.id)*/
+    println(dStream.id)
     val kuduContext = new KuduContext(kuduMaster, spark.sparkContext)
 
     if (!kuduContext.tableExists("impala::tmp.tb_cdc")) {
@@ -60,55 +72,21 @@ object Test {
     }
 
 
-    val testDF = spark.read.format("json").load("/tmp/testcdc.json")
+    /*val testDF = spark.read.format("json").load("/tmp/testcdc.json")
 
     testDF.printSchema()
-    println("---------------------------------id")
-    val beforeDF = testDF.select("payload.before.id","payload.before.first_name","payload.before.last_name","payload.before.email").toDF("id","firstname","lastname","email")
-    beforeDF.show()
-    val afterDF = testDF.select("payload.after.id","payload.after.first_name","payload.after.last_name","payload.after.email").toDF("id","firstname","lastname","email")
-    afterDF.show()
-    /**
-     * root
-     * |-- payload: struct (nullable = true)
-     * |    |-- after: struct (nullable = true)
-     * |    |    |-- email: string (nullable = true)
-     * |    |    |-- first_name: string (nullable = true)
-     * |    |    |-- id: long (nullable = true)
-     * |    |    |-- last_name: string (nullable = true)
-     * |    |-- before: struct (nullable = true)
-     * |    |    |-- email: string (nullable = true)
-     * |    |    |-- first_name: string (nullable = true)
-     * |    |    |-- id: long (nullable = true)
-     * |    |    |-- last_name: string (nullable = true)
-     * |    |-- op: string (nullable = true)
-     * |    |-- source: struct (nullable = true)
-     * |    |    |-- db: string (nullable = true)
-     * |    |    |-- file: string (nullable = true)
-     * |    |    |-- gtid: string (nullable = true)
-     * |    |    |-- name: string (nullable = true)
-     * |    |    |-- pos: long (nullable = true)
-     * |    |    |-- row: long (nullable = true)
-     * |    |    |-- server_id: long (nullable = true)
-     * |    |    |-- snapshot: string (nullable = true)
-     * |    |    |-- table: string (nullable = true)
-     * |    |    |-- thread: long (nullable = true)
-     * |    |    |-- ts_sec: long (nullable = true)
-     * |    |-- ts_ms: long (nullable = true)
-     * |    |-- name: string (nullable = true)
-     * |    |-- optional: boolean (nullable = true)
-     * |    |-- type: string (nullable = true)
-     * |    |-- version: long (nullable = true)
-     */
+    println("---------------------------------id")*/
 
+    /*val operation = testDF.select("payload.op").first().getString(0)
+    println(operation)
+    if (operation == "u" || operation == "c"){
+      val beforeDF = testDF.select("payload.before.id","payload.before.first_name","payload.before.last_name","payload.before.email").toDF("id","firstname","lastname","email")
+      beforeDF.show()
+      val afterDF = testDF.select("payload.after.id","payload.after.first_name","payload.after.last_name","payload.after.email").toDF("id","firstname","lastname","email")
+      afterDF.show()
 
-   /* val kuduDF = spark.createDataFrame(Seq(
-      ("1001","ming","lv","aaa"),
-      ("1002","mhong","zhang","1aa")
-    )) toDF("id", "firstname", "lastname","email")*/
-
-    kuduContext.upsertRows(afterDF, "impala::tmp.tb_cdc")
-
+      kuduContext.upsertRows(afterDF, "impala::tmp.tb_cdc")
+    }*/
 
     /*if (!kuduContext.tableExists("tb_cdc")) {
       println("create Kudu Table :{tb_cdc}")
@@ -116,26 +94,34 @@ object Test {
       createTableOptions.setRangePartitionColumns(List("id"))
       kuduContext.createTable("tb_cdc", cdcSchema, Seq("id"), createTableOptions)
     }*/
-    /*dStream.map(record => record.value()).foreachRDD(rdd => {
+    dStream.map(record => record.value()).foreachRDD(rdd => {
       val spark = SparkSession.builder().config(rdd.sparkContext.getConf).getOrCreate()
       import spark.implicits._
       val cdcDF = spark.read.json(spark.createDataset(rdd))
       cdcDF.printSchema()
-      val plDF = cdcDF.select(explode(cdcDF("payload"))).toDF("pl")
+      /*val operation = cdcDF.select("payload.op").first().getString(0)
+      println(operation)*/
+      //      if (operation == "u" || operation == "c"){
+      val beforeDF = cdcDF.select("payload.before.id","payload.before.first_name","payload.before.last_name","payload.before.email").toDF("id","firstname","lastname","email")
+      beforeDF.show()
+      val afterDF = cdcDF.select("payload.after.id","payload.after.first_name","payload.after.last_name","payload.after.email").toDF("id","firstname","lastname","email")
+      afterDF.show()
+
+      kuduContext.upsertRows(afterDF, "impala::tmp.tb_cdc")
+      //      }
+
+      /*val plDF = cdcDF.select(explode(cdcDF("payload"))).toDF("pl")
       val beforeDF = plDF.select(explode(plDF("before"))).toDF("before")
       val afterDF = plDF.select(explode(plDF("after"))).toDF("after")
       val beforeVal = beforeDF.select("before.id","before.first_name","before.last_name","before.email")
       val afterVal = afterDF.select("after.id","after.first_name","after.last_name","after.email")
       beforeVal.show()
       afterVal.show()
-//      println("print---------------------------------df")
-//      println(cdcDF.describe())
-//      println(cdcDF.head())
-      kuduContext.upsertRows(afterVal, "impala::tmp.tb_cdc")
-    })*/
+      kuduContext.upsertRows(afterVal, "impala::tmp.tb_cdc")*/
+    })
 
-//    ssc.start()
-//    ssc.awaitTermination()
+    ssc.start()
+    ssc.awaitTermination()
 
 
     /*    val spark = SparkSession
